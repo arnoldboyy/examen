377d5cde875c3a2dc568feea0d1f1a3b616390ce
Kevin Arnold Cortés Pacheco <126813663+arnoldboyy@users.noreply.github.com>
2025-05-09 17:34:30 -0600

    Update test_fibonacci.py

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -5,11 +5,9 @@ def test_fibonacci():
     assert fibonacci(0) == 0
     assert fibonacci(1) == 1
     assert fibonacci(5) == 5
-    print("Pruebas pasadas!")
+    print("¡Pruebas pasadas! Código de Kevin Arnold Cortes Pacheco")
 
 if __name__ == "__main__":
     test_fibonacci()
-    time = timeit.timeit("fibonacci(20)",
-                         setup="from fibonacci import fibonacci",
-                         number=100)
-    print(f"Tiempo: {time:.4f} segundos")
+    time = timeit.timeit("fibonacci(20)", setup="from fibonacci import fibonacci", number=100)
+    print(f"Tiempo de ejecución (100 iteraciones de fibonacci(20)): {time:.4f} segundos")
