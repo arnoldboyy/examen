db1b30a610872ea79339fc43cf080f2e4b70e983
Kevin Arnold Cortés Pacheco <126813663+arnoldboyy@users.noreply.github.com>
2025-05-09 17:35:53 -0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,5 @@
-def fibonacci ( n):
- if n <= 1:
- return n
- else :
- return fibonacci (n -1) + fibonacci (n -2)
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
